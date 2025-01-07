55b1e5d8120b196156fe2bc0145bb6ba1f7b040e
Irfan Zidni <irfanzidniofficial@gmail.com>
2025-01-07 15:05:54 +0700

    feat: login implementation by google with convex auth service

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -16,9 +16,13 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
   const { signIn } = useAuthActions();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [pending, setPending] = useState(false);
 
-  const hanldeProviderSignIn = (value: "github" | "google") => {
-    signIn(value);
+  const onProviderSignIn = (value: "github" | "google") => {
+    setPending(true);
+    signIn(value).finally(() => {
+      setPending(false);
+    });
   };
 
   return (
@@ -30,7 +34,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
       <CardContent className="space-y-5 px-0 pb-0">
         <form className="space-y-2.5">
           <Input
-            disabled={false}
+            disabled={pending}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
@@ -38,26 +42,31 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             type="password"
             required
           />
-          <Button type="submit" className="w-full" size="lg" disabled={false}>
+          <Button type="submit" className="w-full" size="lg" disabled={pending}>
             Continue
           </Button>
         </form>
         <Separator />
         <div className="flex flex-col gap-y-2.5">
-          <Button disabled={false} onClick={() => {}} variant="outline" className="w-full relative">
+          <Button
+            disabled={pending}
+            onClick={() => onProviderSignIn("google")}
+            variant="outline"
+            className="w-full relative"
+          >
             <FcGoogle className="size-5 absolute top-2.5 left-2.5 " />
             Continue with Google
           </Button>
           <Button
-            disabled={false}
-            onClick={() => hanldeProviderSignIn("github")}
+            disabled={pending}
+            onClick={() => onProviderSignIn("github")}
             variant="outline"
             className="w-full relative"
           >
